1fba2a356aa5693ed3f3ceb652ea9b6c8ecf5b46
Vishal Sawant <sawantvishal2001@gmail.com>
2026-07-09 18:48:29 +0530

    resolve delete page

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'dart:html' as html;
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:portfolio/screens/DeleteAccount/delete_account_screen.dart';
 import 'package:portfolio/screens/Home/home_screen.dart';
-
 import 'core/theme.dart';
 
 void main() {
-  print("PATH: ${html.window.location.pathname}");
-  print("SEARCH: ${html.window.location.search}");
-  print("HASH: ${html.window.location.hash}");
-
   runApp(const MyApp());
 }
 
@@ -20,20 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Get the query parameter safely
-    final String search = html.window.location.search ?? "";
-
-    // Check if the URL contains ?page=delete-account
-    final bool showDeletePage =
-        search.contains("page=delete-account");
+    // Uri.base works on Flutter web without dart:html
+    // reads the full current URL including query params
+    final String? page = Uri.base.queryParameters['page'];
+    final bool showDeletePage = page == 'delete-account';
 
     return GetMaterialApp(
       title: 'Vishal Sawant | Flutter Developer',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.dark,
-
       home: showDeletePage
-          ? DeleteAccountScreen()
+          ?  DeleteAccountScreen()
           : const HomeScreen(),
     );
   }
